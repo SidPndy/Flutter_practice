b65e92f6e525cc6cdfa0b80b7e169cbfd8823288
SidPndy <80775282+SidPndy@users.noreply.github.com>
2021-05-19 22:39:16 +0545

    Making it better.
    Created better then previous one..

--- flutter_project1_Form/lib/main.dart
+++ flutter_project1_Form/lib/main.dart
@@ -10,20 +10,181 @@ class Login extends StatelessWidget {
     return MaterialApp(
       title: 'Projecy1.try',
       home: Scaffold(
-        backgroundColor: Colors.grey[900],
+        // backgroundColor: Colors.white,
         appBar: AppBar(
-          title: Text('Login'),
           leading: Icon(Icons.login),
-          backgroundColor: Colors.grey[800],
+          title: Text('Sign Up'),
+          backgroundColor: Colors.blue[800],
           elevation: 0, //Shadow like thing.
         ),
-        body: Column(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(33, 10, 0, 0),
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('assets/Pic.PNG'),
+                      radius: 35,
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Text(
+                    'Siddhartha Pandey',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[900],
+                      fontSize: 17,
+                    ),
+                  ),
+                ],
+              ),
+              Divider(
+                height: 30,
+                color: Colors.grey,
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(33, 0, 0, 0),
+                child: Text(
+                  'Nickname',
+                  style: TextStyle(
+                    fontSize: 13,
+                    letterSpacing: 3,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue[800],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(36, 0, 20, 0),
+                child: TextFormField(
+                  /* validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your name';
+                    }
+                    return null;
+                  },*/
+                  // maxLength: 30,
+                  decoration: InputDecoration(
+                    enabledBorder: InputBorder.none,
+                    hintText: 'example: Alexgender',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(33, 5, 0, 0),
+                child: Text(
+                  'E-mail',
+                  style: TextStyle(
+                    fontSize: 13,
+                    letterSpacing: 3,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue[800],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(36, 0, 20, 0),
+                child: TextField(
+                  // maxLength: 30,
+                  decoration: InputDecoration(
+                    enabledBorder: InputBorder.none,
+                    hintText: 'example.Alexgender@email.com',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(33, 5, 0, 0),
+                child: Text(
+                  'Password',
+                  style: TextStyle(
+                    fontSize: 13,
+                    letterSpacing: 3,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue[800],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(36, 0, 20, 0),
+                child: TextField(
+                  // maxLength: 30,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    enabledBorder: InputBorder.none,
+                    hintText: 'q@5swohRd',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(33, 5, 0, 0),
+                child: Text(
+                  'Confirm Password',
+                  style: TextStyle(
+                    fontSize: 13,
+                    letterSpacing: 3,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue[800],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(36, 0, 20, 0),
+                child: TextField(
+                  // maxLength: 30,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    enabledBorder: InputBorder.none,
+                    hintText: 'q@5swohRd',
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: FloatingActionButton.extended(
+                  onPressed: () {},
+
+                  backgroundColor: Colors.blue[900],
+                  label: Text('                Sign Up                  '),
+                  //icon: Icon(Icons.login_sharp,),
+                ),
+              ),
+
+              /* Center(
+                child: RaisedButton.icon(
+                  onPressed: () {},
+                  
+                  color: Colors.blue,
+                  icon: Icon(
+                    Icons.login_rounded,
+                    color: Colors.white,
+                  ),
+                  
+                  label: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      fontSize: 18,
+                      letterSpacing: 3,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),*/
+            ],
+          ),
+        ),
+
+        /*  body: Column(
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: CircleAvatar(
                 backgroundImage: AssetImage('assets/Pic.PNG'),
-                radius: 40,
+                radius: 35,
               ),
             ),
             Divider(
@@ -66,6 +227,7 @@ class Login extends StatelessWidget {
                   height: 1,
                 ),
                 decoration: InputDecoration(
+                  icon: Icon(Icons.mail_outline),
                   hintText: 'name.example@email.com',
                   labelText: 'E-mail',
                   labelStyle: TextStyle(
@@ -82,7 +244,7 @@ class Login extends StatelessWidget {
             ),
             SizedBox(height: 10),
             Padding(
-              padding: const EdgeInsets.fromLTRB(20, 5, 5, 0),
+              padding: EdgeInsets.fromLTRB(20, 5, 5, 0),
               child: TextField(
                 //   maxLength: 35,
                 style: TextStyle(
@@ -133,7 +295,7 @@ class Login extends StatelessWidget {
               ],
             ),
           ],
-        ),
+        ), */
       ),
     );
   }
